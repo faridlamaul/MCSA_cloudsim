4490abf25ab11d0c0ed401b562bead895bfc8ff3
faridlamaul <lamaulfarid71@gmail.com>
2023-06-12 09:24:14 +0700

    initialize random position

--- src/Algorithm/Init.java
+++ src/Algorithm/Init.java
@@ -3,13 +3,16 @@ package Algorithm;
 import java.util.Random;
 
 public class Init {
-    public static double[][] init(int N, int pd) {
-        double[][] x = new double[N][pd];
-        double l = -100;
-        double u = 100;
+    // Method untuk inisialisasi posisi awal CSA
+    public static double[][] init(int NTask, int NVM) {
+        double[][] x = new double[NTask][NVM];
+        // batas bawah dan atas
+        double l = -1;
+        double u = 1;
         Random random = new Random();
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < pd; j++) {
+        for (int i = 0; i < NTask; i++) {
+            for (int j = 0; j < NVM; j++) {
+                // Generate posisi awal CSA secara random
                 x[i][j] = l - (l - u) * random.nextDouble();
             }
         }
