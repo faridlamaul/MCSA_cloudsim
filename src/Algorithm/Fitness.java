f402972dec0943ac39e035f5d4ceaeca804cf985
faridlamaul <lamaulfarid71@gmail.com>
2023-06-12 09:24:52 +0700

    add example fitness func

--- src/Algorithm/Fitness.java
+++ src/Algorithm/Fitness.java
@@ -1,4 +1,15 @@
 package Algorithm;
 
 public class Fitness {
+    public static double[] fitness(double[][] xn, int NTask, int NVM) {
+        double[] ft = new double[NTask];
+        for (int i = 0; i < NTask; i++) {
+            double sum = 0;
+            for (int j = 0; j < NVM; j++) {
+                sum += xn[i][j] * xn[i][j];
+            }
+            ft[i] = sum; // Sphere function
+        }
+        return ft;
+    }
 }
